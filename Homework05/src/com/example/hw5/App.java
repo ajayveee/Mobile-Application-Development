526be6482c6abcce42637b8c9264c517fadfd38c
ndoke <ndoke@uncc.edu>
2015-03-20 16:08:40 -0400

    jygghy

--- Homework05/src/com/example/hw5/App.java
+++ Homework05/src/com/example/hw5/App.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.parse.ParseObject;
-
+//Cmment
 public class App implements Serializable {
 	//declared variables
 	private static final long serialVersionUID = 1L;
